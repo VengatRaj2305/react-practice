616ce12bfc0101fd22a0683a62234d037bfcd814
VengatRaj2305 <vengadesan.mechc@gmail.com>
2024-12-14 07:30:38 +0530

    dev

---
+++ app.js
@@ -0,0 +1,8 @@
+
+
+// const header= document.createElement('h1');
+// header.innerHTML="my name is vengat";
+// document.getElementById('root').append(header);
+const head= React.createElement("h1",{},"am h1 tag");
+const root = document.getElementById('root');
+ReactDOM.render(head,root);
